8d4f71664a612e3954e6c7acf53ea301c5630304
Taylor Huffman <taylor@crucialartists.com>
2022-07-29 19:15:46 -0500

    commit tree 3b32caf0b11057e0f1e19c75ffd15e4c1d76f486

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+    validate :title_is_clickbait?
+
+    TITLES_REG = [
+        /Won't Believe/i,
+        /Sectret/i,
+        /Top \d/i,
+        /Guess/i
+    ]
+
+    def title_is_clickbait?
+        if TITLES_REG.none? { |title_reg| title_reg.match title }
+            errors.add(:title, "Check out the clickbait!")
+        end
+    end
 end
